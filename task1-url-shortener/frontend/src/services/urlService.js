178774f6e3b2dc76e71049aa5d7171065b85852b
Eman Ouda <wzx03401@gmail.com>
2026-07-11 11:56:02 +0300

    fix fronted deploy

--- task1-url-shortener/frontend/src/services/urlService.js
+++ task1-url-shortener/frontend/src/services/urlService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = "https://codealpha-urlshortener-62pb.onrender.com/api/url";
+//const API = "http://localhost:5000/api/url";
+const API = "https://shortly-api-jvsl.onrender.com/api/url";
 
 export const shortenUrl = async (originalUrl) => {
   const response = await axios.post(`${API}/shorten`, {
